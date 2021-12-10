cba4576d507aa9d2ff144e3b315eb8303f1b712f
HuguesBoisdon <hugues.boisdon@cpe.fr>
2021-12-10 10:11:18 +0100

    v0.3

--- funk.py
+++ funk.py
@@ -7,6 +7,7 @@ TODO : ALL
 
 """
 
+from os import close
 from random import randint
 
 def wordChoice():
@@ -15,6 +16,35 @@ def wordChoice():
     content = file.readlines()
     word = content[randint(0, len(content))]
     file.close()
-    return word.upper()
+    return word.upper().strip()
     
-print(wordChoice())
+
+def display(wordDisp):
+
+    print("\n")
+    print(wordDisp)
+    letter = input("Entrez une lettre :").upper()
+
+    return letter
+
+def replaceLetter(letter, word, wordTest, wordDisp):
+    matchCount = 0
+    while(letter in wordTest):
+        lInd = wordTest.index(letter)
+        wordDisp = wordDisp[:lInd] + word[lInd] + wordDisp[lInd +1:]
+        wordTest = wordTest[:lInd] + " " + wordTest[lInd +1:]
+        matchCount += 1
+    return wordTest, wordDisp, matchCount
+
+def getHighscore(path):
+    text = open(path,"r")
+    valRen = text.readlines()[0].strip()
+    text.close()
+    return valRen
+
+def setHighscore(path, score):
+    if(score > int(getHighscore(path))):
+        print("BG !!!! Nouveau highscore !")
+        text = open(path,"w")
+        text.write(str(score))
+        text.close()
--- main.py
+++ main.py
@@ -9,40 +9,60 @@ tryCount = maxTries
 word = ""
 wordTest = ""
 wordDisp = " "
+usedLetters = []
+validLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","X","Y","Z"]
+
+highscorePath = "highscore.txt"
 
 while playing :
 
     if(wordDisp == word):
+        print(word)
         print("Gagné !")
         playing = False
 
     elif(tryCount > 0):
 
         if(newGame):
-
+            print("[[[ HIGHSCORE : "+funk.getHighscore(highscorePath)+" ]]]")
             word = funk.wordChoice()
             wordTest = word
-            wordDisp = word[0] + "_"*(len(word) - 1)
+            wordDisp = "_"*len(word)
             newGame = False
+            wordTest, wordDisp, matchCount = funk.replaceLetter(word[0], word, wordTest, wordDisp)
         
         print("\n")
-        print(word)
-        print(wordDisp)
-        print(word == wordDisp)
-        letter = input("Entrez une lettre :").upper()
+        print("-- Il vous reste " + str(tryCount) + " essais ! --")
+        letter = funk.display(wordDisp)
+        while(not(letter in validLetters) or (letter in usedLetters)):
+            if letter in usedLetters:
+                print("lettre dejà utilisée")
+            if letter not in validLetters:
+                print("t con ou qkoi ?")
+            letter = funk.display(wordDisp)
 
-        matchCount = 0
-        while(letter in wordTest):
+        usedLetters.append(letter)
 
-            lInd = wordTest.index(letter)
-            wordDisp = wordDisp[:lInd] + word[lInd] + wordDisp[lInd +1:]
-            wordTest = wordTest[:lInd] + " " + wordTest[lInd +1:]
-            matchCount += 1
+        wordTest, wordDisp, matchCount = funk.replaceLetter(letter, word, wordTest, wordDisp)
 
         if(matchCount == 0) :
             tryCount -=1
             print("nop")
     else:
         print("Perdu !")
+        print(word)
         playing = False
+
+    if(not playing):
+        print("\n"*5)
+        funk.setHighscore(highscorePath,tryCount)
+        resp = ""
+        while(resp not in ["O","N"]):
+            resp = input("Voulez-vous rejouer ? [O/N] :").upper().strip()
+        if resp == "O":
+            playing = True
+            newGame = True
+            tryCount = maxTries
+            wordDisp = " "
+            usedLetters = []
         
